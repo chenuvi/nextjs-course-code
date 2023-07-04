9e62e2597b204b4d136af94d36fa4c65bfdb7300
chenuvi <chenuvi@outlook.com>
2023-07-04 09:37:01 +0800

    Feat: add about page

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+function about() {
+  return (
+    <div>
+      <h1>Hello Next World!</h1>
+    </div>
+  );
+}
+export default about;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
-export default function HomePage  () {
+export default function HomePage() {
   return (
     <div className={styles.container}>
       <h1>Hello Next World!</h1>
     </div>
-  )
+  );
 }
